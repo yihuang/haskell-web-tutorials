49d687982a56e27f7681d01a006b1272fb60f9d3
yihuang <yi.codeplayer@gmail.com>
2012-04-27 10:39:52 +0800

    use MVar causes concurrency problem, switch to stm.

--- active-cache-wai/JsonService.hs
+++ active-cache-wai/JsonService.hs
@@ -10,15 +10,16 @@ import Data.Aeson.Types (typeMismatch)
 import qualified Data.Vector as V
 import qualified Blaze.ByteString.Builder as B
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad
 import Control.Monad.IO.Class (liftIO)
 
--- | write MVar even if it's already full, atomic only if there are no other writer.
-forcePutMVar :: MVar a -> a -> IO ()
-forcePutMVar var a = do
-    tryTakeMVar var
-    putMVar var a
+-- | write TMVar even if it's already full, atomic only if there are no other writer.
+forcePutTMVar :: TMVar a -> a -> STM ()
+forcePutTMVar var a = do
+    tryTakeTMVar var
+    putTMVar var a
 
 newtype SimpleState = SimpleState Int
     deriving (Generic)
@@ -29,10 +30,10 @@ instance FromJSON SimpleState where
     parseJSON o = typeMismatch "SimpleState" o
 
 -- handle http requests
-app :: MVar SimpleState -> Application
+app :: TMVar SimpleState -> Application
 app state req = case pathInfo req of
     [] -> do
-        d <- liftIO (readMVar state)
+        d <- liftIO $ atomically $ readTMVar state
         return $ response (encode d)
     _ -> return response404
   where
@@ -40,12 +41,12 @@ app state req = case pathInfo req of
     response404 = ResponseBuilder status404 [] mempty
 
 -- update the cache periodically.
-cacher :: String -> MVar SimpleState -> Int -> IO ()
+cacher :: String -> TMVar SimpleState -> Int -> IO ()
 cacher url state interval = forever $ do
     ersp <- try (HTTP.simpleHttp url)
     case ersp of
         Right rsp -> maybe (putStrLn "decode json failed.")
-                           (void . forcePutMVar state)
+                           (void . atomically . forcePutTMVar state)
                            (decode rsp)
         Left err -> print (err::SomeException)
     threadDelay interval
@@ -53,7 +54,7 @@ cacher url state interval = forever $ do
 main :: IO ()
 main = do
     let port = 3000
-    state <- newEmptyMVar
+    state <- newEmptyTMVarIO
     _ <- forkIO (cacher "http://10.10.10.3:9002/" state 1000)
     putStrLn $ "http://localhost:"++show port
     run port (app state)
--- active-cache-wai/Simple.hs
+++ active-cache-wai/Simple.hs
@@ -6,23 +6,24 @@ import Data.Monoid
 import qualified Blaze.ByteString.Builder as B
 import qualified Data.ByteString.Lazy as L
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad
 import Control.Monad.IO.Class (liftIO)
 
--- | write MVar even if it's already full, atomic only if there are no other writer.
-forcePutMVar :: MVar a -> a -> IO ()
-forcePutMVar var a = do
-    tryTakeMVar var
-    putMVar var a
+-- | write TMVar even if it's already full, atomic only if there are no other writer.
+forcePutTMVar :: TMVar a -> a -> STM ()
+forcePutTMVar var a = do
+    tryTakeTMVar var
+    putTMVar var a
 
 type SimpleState = L.ByteString
 
 -- handle http requests
-app :: MVar SimpleState -> Application
+app :: TMVar SimpleState -> Application
 app state req = case pathInfo req of
     [] -> do
-        d <- liftIO (readMVar state)
+        d <- liftIO $ atomically $ readTMVar state
         return $ response d
     _ -> return response404
   where
@@ -30,18 +31,18 @@ app state req = case pathInfo req of
     response404 = ResponseBuilder status404 [] mempty
 
 -- update the cache periodically.
-cacher :: String -> MVar SimpleState -> Int -> IO ()
+cacher :: String -> TMVar SimpleState -> Int -> IO ()
 cacher url state interval = forever $ do
     ersp <- try (HTTP.simpleHttp url)
     case ersp of
-        Right rsp -> void $ forcePutMVar state rsp
+        Right rsp -> void $ atomically $ forcePutTMVar state rsp
         Left err -> print (err::SomeException)
     threadDelay interval
 
 main :: IO ()
 main = do
     let port = 3000
-    state <- newEmptyMVar
+    state <- newEmptyTMVarIO
     _ <- forkIO (cacher "http://10.10.10.3:9002/" state 1000)
     putStrLn $ "http://localhost:"++show port
     run port (app state)
